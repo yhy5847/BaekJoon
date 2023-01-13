830bc0cee5a897ae248774984e93f8f335ac4f2a
yhy5847 <yhy5847@gmail.com>
2023-01-13 13:03:44 +0900

    C++

---
+++ 2739/answer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void)
+{
+	int n;
+
+	std::cin >> n;
+	
+	for (int i = 1; i < 10; ++i)
+	{
+		std::cout << n << " * " << i << " = " << n * i << std::endl;
+	}
+
+	return 0;
+}
