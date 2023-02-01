bd7dea087eac1b91e692405ddfaddaa68e9867b0
yhy5847 <yhy5847@gmail.com>
2023-02-01 16:04:37 +0900

    C++

--- 4673/answer.cpp
+++ 4673/answer.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+using namespace std;
+
+int SelfNumber(int n)
+{
+	int num = n;
+	while (n != 0)
+	{
+		num = num +(n % 10);
+		n /= 10;
+	}
+	return num;
+}
 
 int main(void)
 {
-	for (int i = 1; i <= 10000; ++i)
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	bool check[10001] = { false };
+	for (int i = 1; i < 10001; ++i)
 	{
-		
+		int n = SelfNumber(i);
+		if (n < 10001)
+		{
+			check[n] = true;
+		}
+	}
+
+	for (int i = 1; i < 10001; ++i)
+	{
+		if (!check[i])
+		{
+			cout << i << "\n";
+		}
 	}
 }
